1e5f406948b877691e0e6b8a5a0ed77247da70ef
Aleksei-Uzin <aleksei.uzin@gmail.com>
2024-06-13 22:03:41 +0400

    add application structure: routing and components

---
+++ src/features/posts/index.js
@@ -0,0 +1,6 @@
+export * from './AddPostForm'
+export * from './EditPostForm'
+export * from './PostsList'
+export * from './ReactionButtons'
+export * from './SinglePostPage'
+export * from './TimeAgo'
